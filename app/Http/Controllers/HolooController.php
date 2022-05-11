f2b8b201b5be3cd1c45b314f4ecfa1cfb61f1265
kazemi.milad <kazemi.milad@gmail.com>
2022-05-11 23:36:15 +0430

    a

--- app/Http/Controllers/HolooController.php
+++ app/Http/Controllers/HolooController.php
@@ -188,7 +188,7 @@ class HolooController extends Controller
                     CURLOPT_RETURNTRANSFER => true,
                     CURLOPT_ENCODING => '',
                     CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_TIMEOUT => 5,
                     CURLOPT_FOLLOWLOCATION => true,
                     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                     CURLOPT_CUSTOMREQUEST => 'GET',
