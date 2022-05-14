80d5142bc13b0de428af9869fccc224f9e2e8021
kazemi.milad <kazemi.milad@gmail.com>
2022-05-14 16:47:23 +0430

    a

--- app/Http/Controllers/WCController.php
+++ app/Http/Controllers/WCController.php
@@ -494,7 +494,8 @@ class WCController extends Controller
                                     'wholesale_customer_wholesale_price' => (isset($config->update_product_price) && $config->update_product_price=="1") && (isset($wholesale_customer_wholesale_price) && (int)$wholesale_customer_wholesale_price != $this->get_price_type($config->wholesale_price_field,$HolooProd)) ? $this->get_price_type($config->wholesale_price_field,$HolooProd)  : ((isset($wholesale_customer_wholesale_price)) ? (int)$wholesale_customer_wholesale_price : null),
                                     'stock_quantity' => (isset($config->update_product_stock) && $config->update_product_stock=="1" && (int) $HolooProd->exist>0 and isset($WCProd->stock_quantity)) ? (int) $HolooProd->exist : 0,
                                 ];
-
+                                log::info("add new product to queue");
+                                log::info($data);
                                 //$data=[(int)$WCProd->sale_price  ,$this->get_price_type($config->special_price_field,$HolooProd),((int)$WCProd->sale_price != $this->get_price_type($config->special_price_field,$HolooProd)),$config->special_price_field];
                                 //return $this->sendResponse('همه محصولات به روز رسانی شدند.', Response::HTTP_OK, $data);
                                 $s=UpdateProductsUser::dispatch($user,$data,$wcHolooCode)->onConnection('redis');
