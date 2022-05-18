3391c6e1fddd1a779a57184f34ccd5fe18f7716e
kazemi.milad <kazemi.milad@gmail.com>
2022-05-19 01:24:40 +0430

    a

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/unauthenticated', function () {
 
 
 //assistent
-Route::get('migrate', [WCController::class, 'migrate']);
+//Route::get('migrate', [WCController::class, 'migrate']);
 //Route::get('fresh', [WCController::class, 'fresh']);
 Route::get('cashClear', [WCController::class, 'clearCache']);
 
