27de5386c6e8881500d7595455a1701c8da9c285
kazemi.milad <kazemi.milad@gmail.com>
2022-05-31 13:59:21 +0430

    a

--- app/Http/Controllers/HolooController.php
+++ app/Http/Controllers/HolooController.php
@@ -1710,7 +1710,7 @@ class HolooController extends Controller
     }
 
     public function changeProduct(Request $config){
-
+        log::info($config->all());
         return $this->sendResponse("ویرایش محصول دریافت شد.", Response::HTTP_OK, ["result" => ["msg_code" => 0]]);
     }
 
