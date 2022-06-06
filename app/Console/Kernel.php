d1d210d2e7b2269260d306fcef5049080f02caec
kazemi.milad <kazemi.milad@gmail.com>
2022-06-07 02:04:59 +0430

    a

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends ConsoleKernel
 
 
             }
-        })->name('every day auto update')->withoutOverlapping()->dailyAt('01:00');
+        })->name('every day auto update')->withoutOverlapping()->dailyAt('02:05');
 
     }
 
