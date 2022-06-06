590b776cd29ce7aa04e1da0c030334f14629f83e
kazemi.milad <kazemi.milad@gmail.com>
2022-06-07 02:20:26 +0430

    a

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,12 +34,12 @@ class Kernel extends ConsoleKernel
                 if ($user->config==null) continue;
                 log::info("run auto update night for user: ".$user->id);
                 $config=json_decode($user->config);
-                log::info($config->product_cat);
-                UpdateProductFind::dispatch((object)["id"=>$user->id,"siteUrl"=>$user->siteUrl,"serial"=>$user->serial,"apiKey"=>$user->apiKey,"holooDatabaseName"=>$user->holooDatabaseName,"consumerKey"=>$user->consumerKey,"consumerSecret"=>$user->consumerSecret,"cloudTokenExDate"=>$user->cloudTokenExDate,"cloudToken"=>$user->cloudToken, "holo_unit"=>$user->holo_unit, "plugin_unit"=>$user->plugin_unit],$user->config["product_cat"],$user->config,1)->onQueue("high");
+                //log::info($config->product_cat);
+                UpdateProductFind::dispatch((object)["id"=>$user->id,"siteUrl"=>$user->siteUrl,"serial"=>$user->serial,"apiKey"=>$user->apiKey,"holooDatabaseName"=>$user->holooDatabaseName,"consumerKey"=>$user->consumerKey,"consumerSecret"=>$user->consumerSecret,"cloudTokenExDate"=>$user->cloudTokenExDate,"cloudToken"=>$user->cloudToken, "holo_unit"=>$user->holo_unit, "plugin_unit"=>$user->plugin_unit],$config->product_cat,$config,1)->onQueue("high");
 
 
             }
-        })->name('every day auto update')->withoutOverlapping()->dailyAt('02:16');
+        })->name('every day auto update')->withoutOverlapping()->dailyAt('02:21');
 
     }
 
