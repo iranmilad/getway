92ba3d1b637c6e5584b6298d6e2a0de6b5789590
kazemi.milad <kazemi.milad@gmail.com>
2022-07-07 14:14:58 +0430

    a

--- app/Http/Controllers/WCController.php
+++ app/Http/Controllers/WCController.php
@@ -1514,7 +1514,7 @@ class WCController extends Controller
 
                         $WCProd=$this->getVariationProductWithHoloo($holooID,$WCProd,$holooProduct,$config);
                         if (!$WCProd) {
-                            Log::info("holo code not found variation product in wc for code ".$holooID." for wc product id ".$WCProd->id);
+                            Log::info("holo code not found variation product in wc for code ".$holooID." for wc product id ".$WCProd[0]->id);
                             continue;
                         }
                         Log::info("holo code found variation product ".$holooID);
