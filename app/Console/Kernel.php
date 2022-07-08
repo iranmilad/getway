bd545fe78c913e8a9825383b12d679f5be589f91
kazemi.milad <kazemi.milad@gmail.com>
2022-07-09 01:04:24 +0430

    a

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
+
     /**
      * Define the application's command schedule.
      *
@@ -34,8 +35,9 @@ class Kernel extends ConsoleKernel
                 if ($user->config==null) continue;
                 log::info("run auto update night for user: ".$user->id);
                 $config=json_decode($user->config);
+                $cloudToken= $this->getNewToken($user);
                 //log::info($config->product_cat);
-                UpdateProductFind::dispatch((object)["id"=>$user->id,"siteUrl"=>$user->siteUrl,"serial"=>$user->serial,"apiKey"=>$user->apiKey,"holooDatabaseName"=>$user->holooDatabaseName,"consumerKey"=>$user->consumerKey,"consumerSecret"=>$user->consumerSecret,"cloudTokenExDate"=>$user->cloudTokenExDate,"cloudToken"=>$user->cloudToken, "holo_unit"=>$user->holo_unit, "plugin_unit"=>$user->plugin_unit,"user_traffic"=>$user->user_traffic],$config->product_cat,$config,1)->onQueue("high");
+                UpdateProductFind::dispatch((object)["id"=>$user->id,"siteUrl"=>$user->siteUrl,"serial"=>$user->serial,"apiKey"=>$user->apiKey,"holooDatabaseName"=>$user->holooDatabaseName,"consumerKey"=>$user->consumerKey,"consumerSecret"=>$user->consumerSecret,"cloudTokenExDate"=>$user->cloudTokenExDate,"cloudToken"=>$cloudToken, "holo_unit"=>$user->holo_unit, "plugin_unit"=>$user->plugin_unit,"user_traffic"=>$user->user_traffic],$config->product_cat,$config,1)->onQueue("high");
 
 
             }
@@ -48,8 +50,9 @@ class Kernel extends ConsoleKernel
                 if ($user->config==null) continue;
                 log::info("run auto update night for heavy user: ".$user->id);
                 $config=json_decode($user->config);
+                $cloudToken= $this->getNewToken($user);
                 //log::info($config->product_cat);
-                UpdateProductFind::dispatch((object)["id"=>$user->id,"siteUrl"=>$user->siteUrl,"serial"=>$user->serial,"apiKey"=>$user->apiKey,"holooDatabaseName"=>$user->holooDatabaseName,"consumerKey"=>$user->consumerKey,"consumerSecret"=>$user->consumerSecret,"cloudTokenExDate"=>$user->cloudTokenExDate,"cloudToken"=>$user->cloudToken, "holo_unit"=>$user->holo_unit, "plugin_unit"=>$user->plugin_unit,"user_traffic"=>$user->user_traffic],$config->product_cat,$config,1)->onQueue("high");
+                UpdateProductFind::dispatch((object)["id"=>$user->id,"siteUrl"=>$user->siteUrl,"serial"=>$user->serial,"apiKey"=>$user->apiKey,"holooDatabaseName"=>$user->holooDatabaseName,"consumerKey"=>$user->consumerKey,"consumerSecret"=>$user->consumerSecret,"cloudTokenExDate"=>$user->cloudTokenExDate,"cloudToken"=>$cloudToken, "holo_unit"=>$user->holo_unit, "plugin_unit"=>$user->plugin_unit,"user_traffic"=>$user->user_traffic],$config->product_cat,$config,1)->onQueue("high");
 
 
             }
@@ -68,7 +71,49 @@ class Kernel extends ConsoleKernel
         require base_path('routes/console.php');
     }
 
+    private function getNewToken($user): string
+    {
 
+        $userSerial = $user->serial;
+        $userApiKey = $user->apiKey;
+
+        if ($user->cloudTokenExDate > Carbon::now()) {
+
+            return $user->cloudToken;
+        }
+        else {
+
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://myholoo.ir/api/Ticket/RegisterForPartner',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array('Serial' => $userSerial, 'RefreshToken' => 'false', 'DeleteService' => 'false', 'MakeService' => 'true', 'RefreshKey' => 'false'),
+                CURLOPT_HTTPHEADER => array(
+                    'apikey:' . $userApiKey,
+                ),
+            ));
+
+            $response = curl_exec($curl);
+            curl_close($curl);
+            $response = json_decode($response);
+
+            if ($response) {
+                log::info("take new token request and response");
+                log::info(json_encode($response));
+                return $response->result->apikey;
+            }
+            else {
+                return $user->cloudToken;
+            }
+        }
+    }
 
 
 }
